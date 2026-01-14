ffd5b6091352d2c66505b21ad8da82703f498163
Thomas Ramel <132356535+Sevenlegarstropsympa@users.noreply.github.com>
2026-01-14 15:37:57 +0100

    multiples test

--- shell.c
+++ shell.c
@@ -1,29 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define BUFFER_SIZE 1024 // define char user input
+#define TAILLE_BUFF 1024 //taille du buffer
+#define TAILLE_ARGS 64 //taille du tableau arg
 
 int main(void) {
-    char input[BUFFER_SIZE]; 
-    printf("cmd$ ");
+    char input[TAILLE_BUFF];
+    char *args[TAILLE_ARGS];
+    int i;
+    pid_t pid;
     
-    while (fgets(input, BUFFER_SIZE, stdin)) {
+    printf("$ ");
+    
+    while (fgets(input, TAILLE_BUFF, stdin)) {
+        // suppression du \n
         input[strcspn(input, "\n")] = 0;
         
+        //si y a rien on reprompt
         if (strlen(input) == 0) {
-            printf("cmd$ ");
+            printf("$ ");
             continue;
         }
-
-        if (strcmp(input, "exit") == 0) {
+        
+        //Tokenisation 
+        i = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL && i < TAILLE_ARGS - 1) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+        
+        //si les deux chaines sont similaire on exit
+        if (strcmp(args[0], "exit") == 0) {
             break;
         }
         
+        // si arg[0] est = à cd 
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL) {
+                fprintf(stderr, "cd: argument manquant\n");
+            } else if (chdir(args[1]) != 0) {
+                perror("cd");
+            }
+            printf("$ ");
+            continue;
+        }
+
+        /* Fork + exec
+        pid = fork();
+        if (pid < 0) {
+            perror("fork");
+        } else if (pid == 0) {
+            // Enfant
+            execvp(args[0], args);
+            //execvp a échoué
+            fprintf(stderr, "%s: commande introuvable\n", args[0]);
+            break;
+        } else {
+            // Parent
+            wait(NULL);
+        }*/
         
-        printf("Vous avez saisi: %s\n", input);
-        
-        printf("cmd$ ");
+        printf("$ ");
     }
     
     return 0;
